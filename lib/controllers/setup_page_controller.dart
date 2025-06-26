bd56f23cd4d8011b8d6ae02942144f4babd51358
om1cael <188113439+om1cael@users.noreply.github.com>
2025-06-26 15:56:51 -0300

    feat(setup page): implement validation for input fields

--- lib/controllers/setup_page_controller.dart
+++ lib/controllers/setup_page_controller.dart
@@ -3,4 +3,14 @@ import 'package:flutter/widgets.dart';
 class SetupPageController {
   final TextEditingController daysController = TextEditingController();
   final TextEditingController limitController = TextEditingController();
+
+  bool validate() {
+    int? days = int.tryParse(daysController.text);
+    int? limit = int.tryParse(limitController.text);
+
+    if(days == null || limit == null) return false;
+    if(days < 1 || limit < 1) return false;
+
+    return true;
+  }
 }
